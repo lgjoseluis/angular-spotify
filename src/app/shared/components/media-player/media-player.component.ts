cddef1e2f4de5d6ce6411a658a5ed4d7869ba4a4
José Luis López G <lgjoseluis@hotmail.com>
2022-12-08 19:53:52 -0600

    Add media player with ng-template

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./media-player.component.scss']
 })
 export class MediaPlayerComponent implements OnInit {
+  mockCover: any = {
+    cover:'https://cdn.pixabay.com/photo/2020/08/31/00/33/guitar-5531027_960_720.jpg',
+    album:'Gioli & Assia',
+    name:'BEBE (Oficial)'
+  }
 
   constructor() { }
 
