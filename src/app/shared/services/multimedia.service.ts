d6a79502fdc19b5ed63add5177e0af68bff9b2a6
José Luis López G <jose.lopez@croeminc.com>
2023-09-05 13:52:00 -0600

    Update the time elapsed and remaining of media player component

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -9,6 +9,8 @@ export class MultimediaService {
   public callback:EventEmitter<any> = new EventEmitter<any>();
 
   public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined);
+  public timeElapsed$: BehaviorSubject<string> = new BehaviorSubject('00:00')
+  public timeRemaining$: BehaviorSubject<string> = new BehaviorSubject('-00:00')
   public audio!: HTMLAudioElement;
   
 
@@ -21,7 +23,45 @@ export class MultimediaService {
       {      
         this.setAudio(response);
       }
-    })
+    });
+
+    this.listenAllEvents();
+  }
+  
+  private listenAllEvents(): void
+  {
+    this.audio.addEventListener('timeupdate', this.calculateTime, false);
+  }
+
+  private calculateTime = () =>
+  {
+    const {duration, currentTime} = this.audio;   
+    
+    this.setTimeElapsed(currentTime);
+    this.setTimeRemaining(currentTime, duration);
+  }
+
+  private setTimeElapsed(currentTime: number) : void
+  {
+    let seconds = Math.floor(currentTime % 60);
+    let minutes = Math.floor((currentTime / 60) % 60);
+        
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds;
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes;
+    const displayFormat = `${displayMinutes}:${displaySeconds}`;    
+
+    this.timeElapsed$.next(displayFormat);
+  }
+
+  private setTimeRemaining(currentTime: number, duration: number): void {
+    let timeLeft = duration - currentTime;
+    let seconds = Math.floor(timeLeft % 60);
+    let minutes = Math.floor((timeLeft / 60) % 60);
+
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds;
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes;
+    const displayFormat = `-${displayMinutes}:${displaySeconds}`
+    this.timeRemaining$.next(displayFormat)
   }
 
   public setAudio(track:TrackModel):void
